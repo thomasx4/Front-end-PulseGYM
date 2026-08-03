d32bc6e0201ff3b73407ed2f91610be596c1b9bf
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-03 08:28:22 -0500

    feat: agrego nuevos componentes y corrijo el uth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { CredencialesListado, RegisterRequestDTO, MessageGlobalDTO, HttpGlobalResponse } from '../../features/auth/models/auth/auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = `${environment.apiUrl}/pg-ms-auth/auth`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerCredentials(datos: RegisterRequestDTO): Observable<MessageGlobalDTO> {
+    return this.http.post<MessageGlobalDTO>(`${this.apiUrl}/register`, datos);
+  }
+
+  listarCredenciales(): Observable<HttpGlobalResponse<CredencialesListado[]>> {
+    return this.http.get<HttpGlobalResponse<CredencialesListado[]>>(`${this.apiUrl}/usuarios`);
+  }
+
+  cambiarEstado(id: number, nuevoEstado: boolean): Observable<MessageGlobalDTO> {
+  return this.http.put<MessageGlobalDTO>(
+    `${this.apiUrl}/usuarios/estado/${id}`,
+    { estado: nuevoEstado }
+  );
+}
 }
---
+++ src/app/shared/components/stat-card/stat-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-card',
+  templateUrl: './stat-card.component.html',
+  styleUrl: './stat-card.component.scss'
+})
+export class StatCardComponent {
+
+}
