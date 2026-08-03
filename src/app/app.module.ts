a64349d6e34382ffacb06a614eeabd7c6657b6d0
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-03 08:30:57 -0500

    fix: resuelve conflicto en AuthService al fusionar develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     RouterModule,       
     AppRoutingModule, 
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
