ec696a4edc49c719684cee265c0f96c164b3719d
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-03 06:23:39 -0500

    fix: Sidebar con boton de Users funcional y header con solo la barra de busqueda

--- src/app/shared/components/main-layout/header/header.component.ts
+++ src/app/shared/components/main-layout/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() search = new EventEmitter<string>();
 
+  onSearch(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.search.emit(value);
+  }
 }
