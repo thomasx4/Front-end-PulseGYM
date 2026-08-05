e35d6ed4e044796eb6906db20d43be1883940e56
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-05 16:28:50 -0500

    fix: ajustes en status-badge y conexión en credentials-list

--- src/app/shared/components/status-badge/status-badge.component.ts
+++ src/app/shared/components/status-badge/status-badge.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export type BadgeVarient = 'activo' | 'inactivo';
 
 @Component({
   selector: 'app-status-badge',
   templateUrl: './status-badge.component.html',
   styleUrl: './status-badge.component.scss'
 })
-export class StatusBadgeComponent {
 
+export class StatusBadgeComponent {
+    @Input() texto: string = '';
+    @Input() variante: BadgeVarient = 'inactivo';
 }
