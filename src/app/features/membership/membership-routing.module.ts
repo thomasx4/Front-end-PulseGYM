c1fb4e21217370454f602054428649c2b112313e
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-13 13:57:11 -0500

    feat: agrtego componente en el routing de membresia

--- src/app/features/membership/membership-routing.module.ts
+++ src/app/features/membership/membership-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AssignMembershipComponent } from './components/assign-membership/assign-membership.component';
+import { MembershipListComponent } from './components/membership-list/membership-list.component';
 
 const routes: Routes = [
   {
+    path: 'assign',
+    component: AssignMembershipComponent,
+  },
+  {
+    path: 'list',
+    component: MembershipListComponent,
+  },
+  {
     path: '',
-    component: AssignMembershipComponent
-  }
+    redirectTo: 'assign',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MembershipRoutingModule { }
+export class MembershipRoutingModule {}
