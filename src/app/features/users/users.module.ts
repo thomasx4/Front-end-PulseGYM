575dea5f0756470667c314f8fc8f35983be7d059
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-20 23:45:44 -0500

    feat: agrego estrcutrua del componente de list

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserProfileListComponent } from './components/user-profile-list/user-profile-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserProfileListComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
