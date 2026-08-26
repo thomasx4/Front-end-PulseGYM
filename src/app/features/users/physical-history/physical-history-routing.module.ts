ebf83a3a24fc15d801eee6a1d298a2dbe5b5098e
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-26 09:04:07 -0500

    feat: agrego las rutas de historial fisico

--- src/app/features/users/physical-history/physical-history-routing.module.ts
+++ src/app/features/users/physical-history/physical-history-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhysicalHistoryListComponent } from '../components/physical-history/physical-history-list/physical-history-list.component';
+import { PhysicalHistoryFormComponent } from '../components/physical-history/physical-history-form/physical-history-form.component';
+import { PhysicalHistoryDetailComponent } from '../components/physical-history/physical-history-detail/physical-history-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PhysicalHistoryListComponent },
+  { path: 'new', component: PhysicalHistoryFormComponent },
+  { path: 'edit/:id', component: PhysicalHistoryFormComponent },
+  { path: 'detail/:id', component: PhysicalHistoryDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
