d538c6665a06becc8b60201260fa7f11ebc9d291
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-26 09:01:13 -0500

    feat: agrego intefacte de historial fisico

--- src/app/core/models/physical-history.ts
+++ src/app/core/models/physical-history.ts
@@ -1,2 +1,35 @@
 export interface PhysicalHistory {
+    idHistorialFisico: number;
+    idSocio: number;
+    nombreSocio: string;
+    idRecepcionista?: number;
+    nombreRecepcionista?: string;
+    fechaMedicion: string;
+    pesoKg: number;
+    porcentajeGrasa: number;
+    porcentajeMusculo: number;
+    cinturaCm: number;
+    pechoCm: number;
+    brazoIzqCm?: number;
+    brazoDerCm?: number;
+    piernaIzqCm?: number;
+    piernaDerCm?: number;
+    tendenciaPeso?: 'UP' | 'DOWN' | 'EQUAL';
+    tendenciaGrasa?: 'UP' | 'DOWN' | 'EQUAL';
+    tendenciaMusculo?: 'UP' | 'DOWN' | 'EQUAL';
+}
+
+export interface PhysicalHistoryRequest {
+    idSocio: number;
+    idRecepcionista?: number;
+    fechaMedicion?: string;
+    pesoKg: number;
+    porcentajeGrasa: number;
+    porcentajeMusculo: number;
+    cinturaCm: number;
+    pechoCm: number;
+    brazoIzqCm?: number;
+    brazoDerCm?: number;
+    piernaIzqCm?: number;
+    piernaDerCm?: number;
 }
