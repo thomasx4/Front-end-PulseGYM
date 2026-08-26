d44f4d51fb748715cd76aad689d9173312646689
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-25 22:28:18 -0500

    feat: agrego las interfaces de model de certificates

--- src/app/core/models/certificate.ts
+++ src/app/core/models/certificate.ts
@@ -1,2 +1,29 @@
 export interface Certificate {
+    idCertificacion: number;
+    idEntrenador: number;
+    nombreEntrenador: string;
+    nombreCertificacion: string;
+    urlPdf: string;
+    fechaSubida: string;
+}
+
+export interface CertificateRequest {
+    idEntrenador: number;
+    nombre: string;
+    urlPdf: string;
+}
+
+export interface CertificateUpdate {
+    nombre: string;
+    urlPdf: string;
+}
+
+export interface CertificateFilter {
+    search?: string;
+    certificacion?: string;
+}
+
+export interface CertificateMetric {
+    totalCertificaciones: number;
+    entrenadoresCertificados: number;
 }
