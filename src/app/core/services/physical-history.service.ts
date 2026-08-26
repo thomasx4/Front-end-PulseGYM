6d0ed77e47c77393bf4a8e77043beb11fde6be3d
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-26 09:03:11 -0500

    feat: agrego service de historial fisico

--- src/app/core/services/physical-history.service.ts
+++ src/app/core/services/physical-history.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PhysicalHistory, PhysicalHistoryRequest } from '../models/physical-history';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhysicalHistoryService {
+  private apiUrl = `${environment.apiUrl}/api/v1/usuarios/historial-fisico`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<PhysicalHistory[]> {
+    return this.http.get<PhysicalHistory[]>(this.apiUrl);
+  }
+
+  getBySocio(idSocio: number): Observable<PhysicalHistory[]> {
+    return this.http.get<PhysicalHistory[]>(`${this.apiUrl}/socio/${idSocio}`);
+  }
+
+  create(request: PhysicalHistoryRequest): Observable<any> {
+    return this.http.post<any>(this.apiUrl, request);
+  }
+
+  update(idHistorial: number, request: PhysicalHistoryRequest): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${idHistorial}`, request);
+  }
 }
