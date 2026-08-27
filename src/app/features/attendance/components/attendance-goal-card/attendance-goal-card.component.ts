8c281575ba4c46fcbbdffb89ad5302a4f62c467f
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-27 09:22:43 -0500

    feat: Componente de Tarjeta para Expectativa de Asistencias

--- src/app/features/attendance/components/attendance-goal-card/attendance-goal-card.component.ts
+++ src/app/features/attendance/components/attendance-goal-card/attendance-goal-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-attendance-goal-card',
   templateUrl: './attendance-goal-card.component.html',
   styleUrl: './attendance-goal-card.component.scss'
 })
-export class AttendanceGoalCardComponent {
+export class AttendanceGoalCardComponent implements OnInit {
 
+  @Input() totalAforoActual: number = 0;
+  @Input() metaAsistenciaDiaria: number = 0;
+
+  porcentajeCompletado: number = 0;
+  personasRestantes: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.calcularMetricas();
+  }
+
+  ngOnChanges(): void {
+    this.calcularMetricas();
+  }
+
+  calcularMetricas(): void {
+    if (this.metaAsistenciaDiaria > 0) {
+      const porcentajeRaw = (this.totalAforoActual / this.metaAsistenciaDiaria) * 100;
+      this.porcentajeCompletado = Math.min(Math.round(porcentajeRaw), 100);
+
+      this.personasRestantes = Math.max(this.metaAsistenciaDiaria - this.totalAforoActual, 0);
+    } else {
+      this.porcentajeCompletado = 0;
+      this.personasRestantes = 0;
+    }
+  }
 }
