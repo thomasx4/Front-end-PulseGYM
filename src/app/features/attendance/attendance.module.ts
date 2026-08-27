90168d0d183f80cd85b7ae8a41b33440a57972ba
rixhard-rxp <oskitarmolpa@gmail.com>
2026-08-27 09:23:37 -0500

    feat: Modulo y Model de Asistencias

--- src/app/features/attendance/attendance.module.ts
+++ src/app/features/attendance/attendance.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 import { AttendanceRoutingModule } from './attendance-routing.module';
 import { AttendanceListComponent } from './components/attendance-list/attendance-list.component';
 import { AttendanceHistoryComponent } from './components/attendance-history/attendance-history.component';
+import { AttendanceGoalCardComponent } from './components/attendance-goal-card/attendance-goal-card.component';
 
 
 @NgModule({
   declarations: [
     AttendanceListComponent,
     AttendanceHistoryComponent,
+    AttendanceGoalCardComponent,
   ],
   imports: [
     CommonModule,
-    AttendanceRoutingModule
+    SharedModule,
+    AttendanceRoutingModule,
+    FormsModule,
   ]
 })
 export class AttendanceModule { }
