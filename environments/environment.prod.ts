12b0810640bc76bdfe7e09b319ec980ffa649658
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-31 15:26:55 -0500

    chore: agregar estructura base de environments sin credenciales

---
+++ environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.pulsegym.uk',
+
+    cloudinary: {
+        cloudName: '',
+        uploadPreset: '',
+        apiKey: '',
+        apiSecret: ''
+    }
+};
