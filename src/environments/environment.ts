efe34de7c3a32dbd6a9221aa4b94511e7cd7d0f1
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-31 15:37:04 -0500

    fix: mover environments dentro de src

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.pulsegym.uk',
+
+    cloudinary: {
+        cloudName: '',
+        uploadPreset: '',
+        apiKey: '',
+        apiSecret: ''
+    }
+};
